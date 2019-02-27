80f329d530b34437f705e49d0e3115ebf8d53758
Adam Patridge <patridge@gmail.com>
2019-02-26 21:38:11 -0700

    Unify whitespace.

--- Exercise 1/Start/TipCalculator/TipCalculator/StandardTipPage.xaml.cs
+++ Exercise 1/Start/TipCalculator/TipCalculator/StandardTipPage.xaml.cs
@@ -3,53 +3,53 @@ using Xamarin.Forms;
 
 namespace TipCalculator
 {
-	public partial class StandardTipPage : ContentPage
-	{
-		public StandardTipPage()
-		{
-			InitializeComponent();
-			billInput.TextChanged += (s, e) => CalculateTip();
-		}
+    public partial class StandardTipPage : ContentPage
+    {
+        public StandardTipPage()
+        {
+            InitializeComponent();
+            billInput.TextChanged += (s, e) => CalculateTip();
+        }
 
-		void CalculateTip()
-		{
-			double bill;
+        void CalculateTip()
+        {
+            double bill;
 
-			if (Double.TryParse(billInput.Text, out bill) && bill > 0)
-			{
-				double tip   = Math.Round(bill*0.15, 2);
-				double final = bill + tip;
+            if (Double.TryParse(billInput.Text, out bill) && bill > 0)
+            {
+                double tip   = Math.Round(bill*0.15, 2);
+                double final = bill + tip;
 
-				tipOutput  .Text = tip  .ToString("C");
-				totalOutput.Text = final.ToString("C");
-			}
-		}
+                tipOutput  .Text = tip  .ToString("C");
+                totalOutput.Text = final.ToString("C");
+            }
+        }
 
-		void OnLight(object sender, EventArgs e)
-		{
-			LayoutRoot.BackgroundColor = Color.Silver;
+        void OnLight(object sender, EventArgs e)
+        {
+            LayoutRoot.BackgroundColor = Color.Silver;
 
-			tipLabel   .TextColor = Color.Navy;
-			billLabel  .TextColor = Color.Navy;
-			totalLabel .TextColor = Color.Navy;
-			tipOutput  .TextColor = Color.Navy;
-			totalOutput.TextColor = Color.Navy;
-		}
+            tipLabel   .TextColor = Color.Navy;
+            billLabel  .TextColor = Color.Navy;
+            totalLabel .TextColor = Color.Navy;
+            tipOutput  .TextColor = Color.Navy;
+            totalOutput.TextColor = Color.Navy;
+        }
 
-		void OnDark(object sender, EventArgs e)
-		{
-			LayoutRoot.BackgroundColor = Color.Navy;
+        void OnDark(object sender, EventArgs e)
+        {
+            LayoutRoot.BackgroundColor = Color.Navy;
 
-			tipLabel   .TextColor = Color.Silver;
-			billLabel  .TextColor = Color.Silver;
-			totalLabel .TextColor = Color.Silver;
-			tipOutput  .TextColor = Color.Silver;
-			totalOutput.TextColor = Color.Silver;
-		}
+            tipLabel   .TextColor = Color.Silver;
+            billLabel  .TextColor = Color.Silver;
+            totalLabel .TextColor = Color.Silver;
+            tipOutput  .TextColor = Color.Silver;
+            totalOutput.TextColor = Color.Silver;
+        }
 
-		void GotoCustom(object sender, EventArgs e)
-		{
-			Navigation.PushAsync(new CustomTipPage());
-		}
-	}
+        void GotoCustom(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CustomTipPage());
+        }
+    }
 }
--- Exercise 2/Completed/TipCalculator/TipCalculator/App.xaml.cs
+++ Exercise 2/Completed/TipCalculator/TipCalculator/App.xaml.cs
@@ -2,12 +2,12 @@
 
 namespace TipCalculator
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			InitializeComponent();
-			MainPage = new NavigationPage(new StandardTipPage());
-		}
-	}
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new StandardTipPage());
+        }
+    }
 }
